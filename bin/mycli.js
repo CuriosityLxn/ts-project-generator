043345afa01ca97a49c0a89d3737158314504b13
花间 <huajian@helijia.com>
2021-05-30 17:50:04 +0800

    feat: 添加基本自定义命令-create、start、build

--- bin/mycli.js
+++ bin/mycli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
+const { green } = require('chalk');
 const chalk = require('chalk');
 const colors = ['green', 'blue', 'yellow', 'red'];
 var commander = require('commander');
@@ -12,6 +13,8 @@ colors.forEach(color => {
     isConsole ? console.log(chalk[color](text)) : chalk[color](text);
 });
 
+module.exports = consoleColors;
+
 /* 终端命令-版本号 */
 commander.version('0.0.1');
 
@@ -20,6 +23,27 @@ commander
   .option('-d --debug', 'output extra debugging')
   .option('-h --huajian', 'huajian wants to console');
 
+/* 终端命令-自定义命令  */
+/* create 创建项目 */
+commander
+  .command('create')
+  .description('create a project')
+  .action(() =>
+    consoleColors.green('Welcome to use mycli，it`s easy to build a TS project')
+  );
+
+/* start 运行项目 */
+commander
+  .command('start')
+  .description('project start')
+  .action(() => consoleColors.green('-----运行项目-----'));
+
+/* build 打包项目 */
+commander
+  .command('build')
+  .description('project build')
+  .action(() => consoleColors.green('-----构建项目-----'));
+
 commander.parse(process.argv);
 
 if (commander.debug) {
@@ -27,5 +51,3 @@ if (commander.debug) {
 } else if (commander.huajian) {
   consoleColors.green('option is console');
 }
-
-module.exports = consoleColors;
